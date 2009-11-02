9ba9252703b1bf7b7ef247aea3de37eb17e092ae
Kevin Graney <kgraney@bern.(none)>
2009-11-02 08:01:21 -0500

    Added README and test-1+2 to CM.

---
+++ test-1+2.c
@@ -0,0 +1,14 @@
+/* file: test-1+2.c
+   very small program that prints something to stdout and stderr for testing
+   purposes.
+*/
+#include <stdio.h>
+
+int main()
+{
+  printf("This is to standard output\n");
+  fprintf(stderr, "This is to standard error\n");
+
+  return 0;
+}
+
